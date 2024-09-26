5649610f73fe8364e25b10944a37d885462ac39b
nguyennhan-AI <nhan.nguyenbachkhoa22@hcmut.edu.vn>
2024-09-27 01:32:29 +0700

    hello

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,28 +1,32 @@
-import './App.css';
 import React from 'react';
-import WebcamCapture from './component/WebcamCapture';
-import { useState } from 'react';
-import WebcamSetup from './component/WebcamSetup';
-import "./component/cam-style.css";
+import './project.css';
+import BreadCrumb from './components/BreadCrumb';
+import UserSwitch from './components/UserSwitch';
+import CameraButton from './components/CameraButton.js';
+
 function App() {
-  const [showWebcam,setShowWebcam] = useState(false);
-  const handleCLick = () => {
-    setShowWebcam(true);
-  };
-  // const handleCamera = () => {
-  //   Effect();
-  // }
   return (
-    <div className="App">
-    <button onClick={handleCLick}> Start </button>
-      {showWebcam&&<WebcamCapture></WebcamCapture>}
-        {/* <div className='camera-layer'>  */}
-
-        {/* <button className="camera" >Start Camera</button>
-        </div> */}
-        {showWebcam && <div>You have 4 seconds </div>}
-
+    <div>
+      <header>
+          <h2></h2>
+          <h2 className="name">Binariers</h2>
+          <div className='bar'>        
+          <BreadCrumb></BreadCrumb>
+          <button className="buttonPopup">Login</button>
+          </div>
+      </header>
+      <div className="switch">
+        <UserSwitch></UserSwitch>
       </div>
+      <div className="wrapper"> 
+      <div className="camera-submit">
+        <h2 className="camera-text">Record your camera video</h2>
+        <CameraButton></CameraButton>
+      </div>
+      </div>
+      <div className="response">REPONSE</div>
+    </div>
+
   );
 }
 
