87512e4ae3a245c87dc0387865fd3c74ae1d78d9
nguyennhan-AI <nhan.nguyenbachkhoa22@hcmut.edu.vn>
2024-09-27 12:18:41 +0700

    fucl

--- API/server.py
+++ API/server.py
@@ -3,8 +3,5 @@ from app.app import app
 
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    uvicorn.run('app.app:app', host= '127.0.0.1', port=8080)
-=======
+
     uvicorn.run('app.app:app', host= '127.0.0.1', port=8000)
->>>>>>> origin/main
